ad1d4da92963bb1fbfbc3d6318ecde8ffc4a16a1
Grzegorz <greggkrzykowski@gmail.com>
2018-01-26 21:09:52 -0600

    user mention fix?

--- src/main/java/com/jagrosh/jdautilities/utils/FinderUtil.java
+++ src/main/java/com/jagrosh/jdautilities/utils/FinderUtil.java
@@ -69,7 +69,7 @@ public class FinderUtil
 {
     public final static Pattern DISCORD_ID = Pattern.compile("\\d{17,20}"); // ID
     public final static Pattern FULL_USER_REF = Pattern.compile("(\\S.{0,30}\\S)\\s*#(\\d{4})"); // $1 -> username, $2 -> discriminator
-    public final static Pattern USER_MENTION = Pattern.compile("<@!?(\\d{17,20})>"); // $1 -> ID
+    public final static Pattern USER_MENTION = Pattern.compile("<@?(\\d{17,20})>"); // $1 -> ID
     public final static Pattern CHANNEL_MENTION = Pattern.compile("<#(\\d{17,20})>"); // $1 -> ID
     public final static Pattern ROLE_MENTION = Pattern.compile("<@&(\\d{17,20})>"); // $1 -> ID
     public final static Pattern EMOTE_MENTION = Pattern.compile("<:(.{2,32}):(\\d{17,20})>");
